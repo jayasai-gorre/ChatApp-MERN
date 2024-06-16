175b6789c0c94463dd5f903785b9f57ba64b5d97
Jaya Sai Gorre <123870905+jayasai-gorre@users.noreply.github.com>
2024-06-17 01:41:28 +0530

    link updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatapp-mern-7t9d.onrender.com", {
+			const socket = io("https://chatapp-mern-rkp1.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
